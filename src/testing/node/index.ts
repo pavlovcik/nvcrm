cb0af938f2dceb20e5361d6c4c3e404ca830fb5f
Alexander Pavlovcik <alexander@pavlovcik.com>
2019-11-28 18:01:20 +0800

    Smooth builds

--- src/testing/node/index.ts
+++ src/testing/node/index.ts
@@ -1,31 +1,16 @@
 import path from "path"
 import express from "express"
 import bodyParser from "body-parser";
-import handler from "./crm";
-
-const morgan = require("morgan");
-
-
+import handler from "./post";
 
 const port = 8000;
 const server = express();
 
-server.use(morgan('tiny'))
 server.use(bodyParser.json());
 
-// let pathname = path.resolve(__dirname, `static`);
-let cwd = process.cwd();
 let pathname = `static`;
 let bridge = path.join(`dist`, `testing`, `node`);
 let final = path.join(bridge, pathname);
-
-// pathname = encodeURI(pathname);
-console.log({
-    pathname,
-    cwd,
-    bridge,
-    final
- });
 
 server.use(express.static(final));
 server.post('/crm/', handler);
